3a9d20655c31ae59e3766d07c09433f6ace90ede
Web Dev <shakhzodbeksharipov2002@gmail.com>
2022-01-05 03:54:35 +0500

    corrected

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 
-
 let accountInfo = document.getElementById("account-info");
 let accountClick = document.getElementById("account-click");
 let accountIsOpen = false;
 
 accountClick.addEventListener("click", ()=> {
-    if(!accountIsOpen){
+    if(accountIsOpen){
         accountInfo.style.display = "none";
-        accountIsOpen = true;
+        accountIsOpen = false;
     }else{
         accountInfo.style.display = "block";
-        accountIsOpen =  false;
+        accountIsOpen =  true;
     }
 })
 
@@ -22,14 +21,16 @@ let statusCategory = document.getElementById("statusCategory");
 let statusIsOpen = false;
 
 statusClick.addEventListener("click", ()=> {
-    if(!statusIsOpen){
+    if(statusIsOpen){
         statusCategory.style.display = "none";
-        statusIsOpen = true;
+        statusIsOpen = false;
     }else{
         statusCategory.style.display = "flex";
-        statusIsOpen =  false;
+        statusIsOpen =  true;
     }
 })
+
+
 
 
 
